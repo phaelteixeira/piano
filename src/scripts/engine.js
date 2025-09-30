aa7968925cb2b217897d234ca33ac6b232b81c1d
Raphael <rt_phaelniggaz@live.com>
2025-09-30 14:17:04 -0300

    Created a virtual piano

---
+++ src/scripts/engine.js
@@ -0,0 +1,27 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+
+let mapedKeys = [];
+
+let audio = new Audio("src/tunes/a.wav");
+
+const playTune = (key) => {
+  audio.src = `src/tunes/${key}.wav`;
+  audio.play();
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+  clickedKey.classList.add("active");
+  setInterval(() => {
+    clickedKey.classList.remove("active");
+  }, 150);
+};
+
+pianoKeys.forEach((key) => {
+  mapedKeys.push(key.dataset.key);
+  key.addEventListener("click", () => playTune(key.dataset.key));
+});
+
+document.addEventListener("keydown", (e) => {
+  if (mapedKeys.includes(e.key)) {
+    playTune(e.key);
+  }
+});
