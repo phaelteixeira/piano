00d757b72e36027ad100d3c6c053282cd5212424
Raphael <rt_phaelniggaz@live.com>
2025-09-30 14:29:07 -0300

    volume control increment

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,4 +1,5 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
 
 let mapedKeys = [];
 
@@ -25,3 +26,9 @@ document.addEventListener("keydown", (e) => {
     playTune(e.key);
   }
 });
+
+const handleVolume = (e) => {
+  audio.volume = e.target.value;
+};
+
+volumeSlider.addEventListener("input", handleVolume);
